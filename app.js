4ae5a769f52e18aa806f682584873dd6ffe0f634
saloni2805 <salonispotdar@gmail.com>
2025-09-16 22:18:13 +0530

    data_types

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+// --- > Tuple
+// let arr: [string, number] = ["harsh", 22]
+Object.defineProperty(exports, "__esModule", { value: true });
+// -----------------------
+// ----> Enum
+// enum UserRoles {
+//   Admin = "admin",
+//   Guest = "guest",
+//   Super_admin = "super_admin",
+// }
+// UserRoles.Admin
+// -----------------------
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,16 @@
+// --- > Tuple
+// let arr: [string, number] = ["harsh", 22]
+
+// -----------------------
+
+// ----> Enum
+
+// enum UserRoles {
+//   Admin = "admin",
+//   Guest = "guest",
+//   Super_admin = "super_admin",
+// }
+
+// UserRoles.Admin
+
+// -----------------------
